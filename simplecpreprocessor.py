51e6d1dfb7bf4af66cb1ce3592782f44cffc0ea9
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2015-11-08 20:44:53 +0200

    Refactor to have a place to implement multiline define in

--- simplecpreprocessor.py
+++ simplecpreprocessor.py
@@ -16,6 +16,47 @@ class ParseError(Exception):
     pass
 
 
+def verify_no_multiline_define(multiline_define):
+    if multiline_define:
+        define, line_num = multiline_define
+        s = "Error, expected multiline define %s on line %s to be continued"
+        raise ParseError(s % (define, line_num))
+
+def process_define(line, line_num, multiline_define, defines):
+    verify_no_multiline_define(multiline_define)
+    if line.endswith("\\"):
+        return line, line_num
+    define = line.split(" ", 2)[1:]
+    if len(define) == 1:
+        defines[define[0]] = ""
+    else:
+        for candidate in defines:
+            if candidate in define[1]:
+                r = "Indirect self-reference detected #define %s %s, line %s"
+                raise ParseError(r % (define[0], define[1], line_num))
+        defines[define[0]] = define[1]
+
+def process_endif(line, line_num, multiline_define, constraints):
+    verify_no_multiline_define(multiline_define)
+    if not constraints:
+        raise ParseError("Unexpected #endif on line %s" % line_num)
+    constraints.pop()
+
+def process_if(line, line_num, multiline_define, constraints, if_type):
+    verify_no_multiline_define(multiline_define)
+    _, condition = line.split(" ")
+    constraints.append((condition, if_type, line_num))
+
+def process_undef(line, multiline_define, defines):
+    verify_no_multiline_define(multiline_define)
+    _, undefine = line.split(" ")
+    try:
+        del defines[undefine]
+    except KeyError:
+        pass
+    
+
+
 def preprocess(iterable, line_ending="\n"):
     r"""
     This preprocessor yields lines with \n at the end
@@ -23,39 +64,24 @@ def preprocess(iterable, line_ending="\n"):
     defines = {}
     constraints = []
     ignore = calculate_ignore(defines, constraints)
+    multiline_define = None
     for line_num, line in enumerate(iterable):
         line = line.rstrip("\r\n").rstrip("\n")
         if line == "#endif":
-            if not constraints:
-                raise ParseError("Unexpected #endif on line %s" % line_num)
-            constraints.pop()
+            process_endif(line, line_num, multiline_define, constraints)
             ignore = calculate_ignore(defines, constraints)
         elif line.startswith("#ifdef"):
-            _, define = line.split(" ")
-            constraints.append((define, True, line_num))
+            process_if(line, line_num, multiline_define, constraints, True)
             ignore = calculate_ignore(defines, constraints)
         elif line.startswith("#ifndef"):
-            _, define = line.split(" ")
-            constraints.append((define, False, line_num))
+            process_if(line, line_num, multiline_define, constraints, False)
             ignore = calculate_ignore(defines, constraints)
         elif not ignore:
             if line.startswith("#define"):
-                define = line.split(" ", 2)[1:]
-                if len(define) == 1:
-                    defines[define[0]] = ""
-                else:
-                    for candidate in defines:
-                        if candidate in define[1]:
-                            r = "Indirect self-reference detected #define %s %s, line %s"
-                            raise ParseError(r % (define[0], define[1], line_num))
-                    defines[define[0]] = define[1]
+                multiline_define = process_define(line, line_num, multiline_define, defines)
                 ignore = calculate_ignore(defines, constraints)
             elif line.startswith("#undef"):
-                _, undefine = line.split(" ")
-                try:
-                    del defines[undefine]
-                except KeyError:
-                    pass
+                process_undef(line, multiline_define, defines)
                 ignore = calculate_ignore(defines, constraints)
             elif line.startswith("#"):
                 raise ParseError("%s contains unsupported macro" % line)
