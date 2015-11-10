1a60fd0c07282471301a028bb37d8ed70e0d5a43
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2015-11-10 22:59:37 +0200

    This resolves #1 and adds license file to the distributable

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "simplecpreprocessor",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Seppo Yli-Olli",
     author_email = "seppo.yli-olli@iki.fi",
     description = "Simple C preprocessor for usage eg before CFFI",
@@ -11,7 +11,7 @@ setup(
     url = "http://packages.python.org/simplepreprocessor",
     py_modules=["simplepreprocessor"],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
         ],
