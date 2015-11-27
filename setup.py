68e7fda7c309cb5ee2f826cdf18b745d829e957d
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2015-11-27 17:41:42 +0200

    Fix packaging

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name = "simplecpreprocessor",
-    version = "0.0.6",
+    version = "0.0.7",
     author = "Seppo Yli-Olli",
     author_email = "seppo.yli-olli@iki.fi",
     description = "Simple C preprocessor for usage eg before CFFI",
     keywords = "python c preprocessor",
     license = "BSD",
     url = "https://github.com/nanonyme/simplecpreprocessor",
-    py_modules=["simplepreprocessor"],
+    py_modules=["simplecpreprocessor"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
