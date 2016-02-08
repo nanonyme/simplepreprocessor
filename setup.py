e8ed1008b5c6a26245b9190c5546af4a9f7465b5
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2016-02-09 00:01:33 +0200

    Version 0.0.8 now that the scoping bug is fixed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "simplecpreprocessor",
-    version = "0.0.7",
+    version = "0.0.8",
     author = "Seppo Yli-Olli",
     author_email = "seppo.yli-olli@iki.fi",
     description = "Simple C preprocessor for usage eg before CFFI",
