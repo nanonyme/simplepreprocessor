6feae7f7a9da6f671ca6125589654307fd63cba2
Seppo Yli-Olli <seppo.yli-olli@iki.fi>
2016-02-11 20:48:26 +0200

    Add all files

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup
+import json
+
+with open("version.json") as f:
+    version = ".".join(json.load(f))
+
+long_description="""TravisCI results                                        
+    .. image:: https://travis-ci.org/nanonyme/simplecpreprocessor.svg?tag=v%s
+""" % version
+
 
 setup(
     name = "simplecpreprocessor",
-    version = "0.1.2",
+    version = version,
     author = "Seppo Yli-Olli",
     author_email = "seppo.yli-olli@iki.fi",
     description = "Simple C preprocessor for usage eg before CFFI",
@@ -10,8 +19,7 @@ setup(
     license = "BSD",
     url = "https://github.com/nanonyme/simplecpreprocessor",
     py_modules=["simplecpreprocessor"],
-    long_description="""TravisCI results 
-    .. image:: https://travis-ci.org/nanonyme/simplecpreprocessor.svg?branch=master""",
+    long_description=long_description,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
