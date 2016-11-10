dd2dde743274c6d3003228882344640bc6c16241
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2016-11-10 23:09:29 +0200

    Implement command line interface

--- simplecpreprocessor.py
+++ simplecpreprocessor.py
@@ -1,9 +1,9 @@
 import logging
-from collections import OrderedDict
 import os.path
 import platform
 import errno
 import re
+import argparse
 
 logger = logging.getLogger(__name__)
 
@@ -255,3 +255,26 @@ def preprocess(f_object, line_ending="\n", include_paths=(),
     preprocessor = Preprocessor(line_ending, include_paths, header_handler,
                                 platform_constants, ignore_headers)
     return preprocessor.preprocess(f_object)
+
+def split_paths(path):
+    return path.split(os.pathsep)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input-file", required=True,
+                    help="Header file to parse. Can also be a shim header")
+parser.add_argument("--include-path", action="append",
+                    help="Include paths", dest="include_paths",
+                    default=[])
+parser.add_argument("--ignore-header", action="append",
+                    help="Headers to ignore. Useful for eg CFFI",
+                    dest="ignore_headers", default=[])
+parser.add_argument("--output-file", required=True,
+                    help="Output file that contains preprocessed header(s)")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    with open(args.input_file) as i:
+        with open(args.output_file, "w") as o:
+            for line in preprocess(i, include_paths=args.include_paths,
+                                   ignore_headers=args.ignore_headers):
+                o.write(line)
