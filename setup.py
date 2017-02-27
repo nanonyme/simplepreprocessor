8c55cdb24449e7cdd85c0cab48b1b54111225498
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2017-02-27 20:49:02 +0200

    Move to using setuptools_scm

--- setup.py
+++ setup.py
@@ -1,27 +1,13 @@
 from __future__ import absolute_import
 from setuptools import setup
-import json
-import version_handling
-import errno
-
-try:
-    with open("version.txt") as f:
-        version = f.read()
-except IOError as e:
-    if e.errno != errno.ENOENT:
-        raise
-    version = "%s.%s.%s" % version_handling.get_version()
-    with open("version.txt", "w") as f:
-        f.write(version)
 
 long_description="""TravisCI results                                        
-    .. image:: https://travis-ci.org/nanonyme/simplecpreprocessor.svg?tag=v%s
-""" % version
+    .. image:: https://travis-ci.org/nanonyme/simplecpreprocessor.svg
+"""
 
 
 setup(
     name = "simplecpreprocessor",
-    version = version,
     author = "Seppo Yli-Olli",
     author_email = "seppo.yli-olli@iki.fi",
     description = "Simple C preprocessor for usage eg before CFFI",
@@ -30,6 +16,8 @@ setup(
     url = "https://github.com/nanonyme/simplecpreprocessor",
     py_modules=["simplecpreprocessor"],
     long_description=long_description,
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
