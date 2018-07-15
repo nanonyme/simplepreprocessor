713808075f9f28a108bc0563246a7111b14deb43
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2018-07-15 22:50:58 +0300

    Add specific unit tests for platform

--- simplecpreprocessor/test_preprocessor.py
+++ simplecpreprocessor/test_preprocessor.py
@@ -2,15 +2,23 @@ from __future__ import absolute_import
 import pytest
 from simplecpreprocessor import preprocess
 from simplecpreprocessor.core import Preprocessor
-from simplecpreprocessor.exceptions import ParseError
+from simplecpreprocessor.exceptions import ParseError, UnsupportedPlatform
 from simplecpreprocessor.tokens import Token
+from simplecpreprocessor.platform import (calculate_platform_constants,
+                                          extract_platform_spec)
 from simplecpreprocessor.filesystem import FakeFile, FakeHandler
 import posixpath
 import os
 import cProfile
 from pstats import Stats
+import platform
+import mock
 
 profiler = None
+
+extract_platform_spec_path = ("simplecpreprocessor.platform."
+    "extract_platform_spec"
+)
 
 
 def setup_module(module):
@@ -557,3 +565,55 @@ def test_repeated_macro():
         'A A\n', ])
     ret = preprocess(f_obj)
     assert "".join(ret) == "value value\n"
+
+def test_platform():
+    with mock.patch(extract_platform_spec_path) as mock_spec:
+        mock_spec.return_value = "Linux", "32bit"
+        assert calculate_platform_constants() == {
+            "__linux__": "__linux__",
+            "__i386__": "1",
+            "__i386": "1",
+            "i386": "1",
+            "__SIZE_TYPE__": "size_t",
+        }
+
+        mock_spec.return_value = "Linux", "64bit"
+        assert calculate_platform_constants() == {
+            "__linux__": "__linux__",
+            "__x86_64__": "1",
+            "__x86_64": "1",
+            "__amd64__": "1",
+            "__amd64": "1",
+            "__SIZE_TYPE__": "size_t",
+        }
+
+        mock_spec.return_value = "Windows", "32bit"
+        assert calculate_platform_constants() == {
+            "_WIN32": "1",
+            "__SIZE_TYPE__": "size_t",
+        }
+
+        mock_spec.return_value = "Windows", "64bit"
+        assert calculate_platform_constants() == {
+            "_WIN64": "1",
+            "__SIZE_TYPE__": "size_t",
+        }
+
+        with pytest.raises(UnsupportedPlatform) as excinfo:
+            mock_spec.return_value = "Linux", "128bit"
+            calculate_platform_constants()
+        assert "Unsupported bitness" in str(excinfo.value)
+
+        with pytest.raises(UnsupportedPlatform) as excinfo:
+            mock_spec.return_value = "Windows", "128bit"
+            calculate_platform_constants()
+        assert "Unsupported bitness" in str(excinfo.value)
+
+        with pytest.raises(UnsupportedPlatform) as excinfo:
+            mock_spec.return_value = "The Engine", "32it"
+            calculate_platform_constants()
+        assert "Unsupported platform" in str(excinfo.value)
+                                             
+    system = platform.system()
+    bitness, _ = platform.architecture()
+    assert extract_platform_spec() == (system, bitness)
