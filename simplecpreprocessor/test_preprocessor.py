cbae1d4e0ca9e39f69ff8f0615b8ef477fd9c802
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2021-06-02 20:16:03 +0300

    It's expected that on Windows CALLBACK is __stdcall

--- simplecpreprocessor/test_preprocessor.py
+++ simplecpreprocessor/test_preprocessor.py
@@ -692,12 +692,14 @@ def test_platform():
         assert calculate_platform_constants() == {
             "_WIN32": "1",
             "__SIZE_TYPE__": "size_t",
+            "CALLBACK": "__stdcall"
         }
 
         mock_spec.return_value = "Windows", "64bit"
         assert calculate_platform_constants() == {
             "_WIN64": "1",
             "__SIZE_TYPE__": "size_t",
+            "CALLBACK": "__stdcall"
         }
 
         with pytest.raises(UnsupportedPlatform) as excinfo:
